5abbb5a152d3884c51a4c17eb41d653e3cad8da3
Joan Fisbein <joan.fisbein@clarity.ai>
2020-01-17 11:57:15 +0100

    WIP: New release script

---
+++ release.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+export VERSION=$1
+
+if [[ -z "${VERSION}" ]]; then
+  echo "ERROR: Version is undefined"
+  exit 1
+fi
+if [[ -z "${SONATYPE_USERNAME}" ]]; then
+  echo "ERROR: SONATYPE_USERNAME is undefined"
+  exit 2
+fi
+if [[ -z "${SONATYPE_PASSWORD}" ]]; then
+  echo "ERROR: SONATYPE_PASSWORD is undefined"
+  exit 3
+fi
+if [[ -z "${GPG_KEYNAME}" ]]; then
+  echo "ERROR: GPG_KEYNAME is undefined"
+  exit 4
+fi
+if [[ -z "${GPG_PASSPHRASE}" ]]; then
+  echo "ERROR: GPG_PASSPHRASE is undefined"
+  exit 5
+fi
+
+#git pull
+mvn versions:set -DnewVersion="${VERSION}" -DgenerateBackupPoms=false
+mvn clean deploy --batch-mode --activate-profiles release --settings "${DIR}/settings.xml"
+git checkout .
+git tag "${VERSION}"
+git push tags
